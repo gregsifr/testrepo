81f3f9cee5e6adc7b67dd7b07b101db8314128d3
FullName <E@mail.com>
2023-05-16 22:22:23 +0400

    Update

--- pythonppw/__init__.py
+++ pythonppw/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """FullName"""
 __email__ = "E@mail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.1"
--- pythonppw/app.py
+++ pythonppw/app.py
@@ -1,1 +1,8 @@
 """Main module."""
+def test(text: str):
+    """Amazing function
+
+    Args:
+        text (str): What text to print
+    """
+    print(text)
